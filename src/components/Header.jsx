aebab42a90452e00af6148c37685c6a3bb79b35a
KatanovaLesya <katanovalesya@gmail.com>
2025-07-23 14:51:09 +0300

    +

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
 
 import styles from "./Header.module.css";
 import UserInfo from "./UserInfo"; 
+import PropTypes from "prop-types";
 
 const Header = ({ user, onLogout }) => (
   <header className={styles.header}>
-    <UserInfo user={user} onLogout={handleLogout} />
+    <UserInfo user={user} onLogout={onLogout} />
   </header>
 );
 
+Header.propTypes = {
+  user: PropTypes.shape({
+    avatarURL: PropTypes.string,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+    telegram: PropTypes.string,
+    roles: PropTypes.arrayOf(PropTypes.string)
+  }),
+  onLogout: PropTypes.func
+};
 export default Header;
