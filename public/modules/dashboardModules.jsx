eac7a2aa971c2dba3828548030a0cde29432b2d1
KatanovaLesya <katanovalesya@gmail.com>
2025-07-26 00:58:16 +0300

    +

---
+++ public/modules/dashboardModules.jsx
@@ -0,0 +1,62 @@
+export const DASHBOARD_MODULES = [
+  {
+    name: "Заявка",
+    image: "/modules/eagle3.jpg",
+    link: "/request"
+  },
+  {
+    name: "Монітор заявок",
+    image: "/modules/cat2.jpg",
+    link: "/requests-monitor"
+  },
+  {
+    name: "Клієнти",
+    image: "/modules/dog.jpg",
+    link: "/clients"
+  },
+  {
+    name: "Об’єми робіт",
+    image: "/modules/pile.jpg",
+    link: "/work-volumes"
+  },
+  {
+    name: "Звіти керівника підрозділу",
+    image: "/modules/lion.jpg",
+    link: "/department-reports"
+  },
+  {
+    name: "Нормативні дані",
+    image: "/modules/horse2.jpg",
+    link: "/norms"
+  },
+  {
+    name: "Узгодження вартості",
+    image: "/modules/crocodile.jpg",
+    link: "/cost-approval"
+  },
+  {
+    name: "Контроль оборотів",
+    image: "/modules/bear.jpg",
+    link: "/turnover-control"
+  },
+  {
+    name: "Розрахунок доходів",
+    image: "/modules/fox-mongoose.jpg",
+    link: "/income-calc"
+  },
+  {
+    name: "Банк",
+    image: "/modules/hamster.jpg",
+    link: "/bank"
+  },
+  {
+    name: "Взаєморозрахунки",
+    image: "/modules/owl.jpg",
+    link: "/settlements"
+  },
+  {
+    name: "Звіти керівника компанії",
+    image: "/modules/elephant.jpg",
+    link: "/company-reports"
+  },
+];
