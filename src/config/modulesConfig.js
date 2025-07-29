85dc9c8dca42d24a6242d40688c67dd1b39f760b
KatanovaLesya <katanovalesya@gmail.com>
2025-07-29 15:51:52 +0300

    +

--- src/config/modulesConfig.js
+++ src/config/modulesConfig.js
@@ -6,5 +6,91 @@ export const modules = [
     roles: ["admin"],
     description: "Керування користувачами та ролями"
   },
+    
+  {
+    name: "Заявка",
+    image: "/images/eagle3.jpg",
+    path: "",
+    roles: ["sales", "admin"],
+    description: "Робота з потенційним клієнтом"
+   
+  },
+  {
+    name: "Монітор заявок",
+    image: "/images/cat2.jpg",
+    path: "",
+    roles: ["sales", "admin"],
+    description: "Відслідковування статусу заявок"
+  },
+  {
+    name: "Клієнти",
+    image: "/images/dog.jpg",
+    path: "",
+    roles: ["sales", "admin", "expert", "chief", "commercialist"],
+    description: "Відслідковування статусу заявок"
+  },
+  {
+    name: "Об’єми робіт",
+    image: "/images/pile.jpg",
+    path: "",
+    roles: ["admin", "expert", "chief"],
+    description: "Форми для уточнення обсягів робіт по клієнтам"
+  },
+  {
+    name: "Звіти керівника підрозділу",
+    image: "/images/lion.jpg",
+    path: "",
+    roles: ["admin", "chief"],
+    description: "Показники для керівника основного департаменту"
+  },
+  {
+    name: "Нормативні дані",
+    image: "/images/horse2.jpg",
+    path: "",
+    roles: ["admin"],
+    description: "Нормативні дані для розрахунків"
+  },
+  {
+    name: "Узгодження вартості",
+    image: "/images/crocodile.jpg",
+    path: "",
+    roles: ["admin", "commercialist"],
+    description: "Узгодження вартості послуг для виставлення рахунків"
+  },
+  {
+    name: "Контроль оборотів",
+    image: "/images/bear.jpg",
+    path: "",
+    roles: ["admin", "expert", "chief"],
+    description: "Контроль лімітів по оборотам клієнтів"
+  },
+  {
+    name: "Розрахунок доходів",
+    image: "/images/fox-mongoose.jpg",
+    path: "",
+    roles: ["admin", "expert", "chief"],
+    description: "Розрахунок доходів клієнтів"
+  },
+  {
+    name: "Банк",
+    image: "/images/hamster.jpg",
+    path: "",
+    roles: ["sales", "admin", "expert", "chief", "commercialist"],
+    description: "Оплати від клієнтів"
+  },
+  {
+    name: "Взаєморозрахунки",
+    image: "/images/owl.jpg",
+    path: "",
+    roles: ["sales", "admin", "expert", "chief", "commercialist"],
+    description: "Взаєморозрахунки з клієнтами"
+  },
+  {
+    name: "Звіти керівника компанії",
+    image: "/images/elephant.jpg",
+    path: "",
+    roles: ["admin", "commercialist"],
+    description: "Показники для керівника компанії"
+  },
  
 ];
