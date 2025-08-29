49aa45ea266f4791c80b8af81bcdf033084603ab
KatanovaLesya <katanovalesya@gmail.com>
2025-08-29 20:17:07 +0300

    +

---
+++ src/pages/ClientsPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/ClientsPage.jsx
+
+const ClientsPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Клієнти</h1>
+      <p>Сторінка в розробці...</p>
+    </div>
+  );
+};
+
+export default ClientsPage;
