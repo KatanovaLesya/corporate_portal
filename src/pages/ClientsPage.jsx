2e04e2f996c86b591f29698645b64a941a9c6a60
KatanovaLesya <katanovalesya@gmail.com>
2025-09-05 11:14:55 +0300

    +

--- src/pages/ClientsPage.jsx
+++ src/pages/ClientsPage.jsx
@@ -73,9 +73,10 @@ export default function ClientsPage() {
           ...filters,
         },
       });
+        
+      console.log("rawClients ===>", res.data.rows || res.data);
 
       const rawClients = res.data.rows || [];
-      console.log("rawClients ===>", res.data.rows || res.data);
       setRows(normalizeClients(rawClients));
       setCount(res.data.count || 0);
     } catch (err) {
