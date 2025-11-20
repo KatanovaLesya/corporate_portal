7d0ac0016f384f42aedfd64533ff556f966834f4
KatanovaLesya <katanovalesya@gmail.com>
2025-11-20 18:18:29 +0200

    +

--- src/pages/NormativeDataPage.jsx
+++ src/pages/NormativeDataPage.jsx
@@ -1,2 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export default function NormativeDataPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2></h2>
+      <p>Тут будуть таблиці з параметрами для розрахунків.</p>
+    </div>
+  );
+}
+
 
